12fd38803d83c13f6a365ee769fcb8e50d5e1431
lasseabelsen <lasseabelsen@gmail.com>
2015-05-22 15:04:25 +0200

    Base Project added

---
+++ PortFolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortFolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Something about me";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Call me or write me an email, I will get back to you as soon as possible";
+
+            return View();
+        }
+    }
+}
---
+++ PortFolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PortFolio.Startup))]
+namespace PortFolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
