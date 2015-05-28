bdd54088741b6cb4a9a0d89db3126c8dc1fd7547
lasseabelsen <lasseabelsen@gmail.com>
2015-05-28 12:39:26 +0200

    for loop images added

---
+++ PortFolio/Controllers/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace PortFolio.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult SingleImage()
+        {
+            ViewBag.Image = "/Content/Images/Image1.bmp";
+
+            return View();
+        }
+
+        public ActionResult ImageList()
+        {
+            string[] fileNames = Directory.GetFiles(@"\\abdfs01\ab\l\laab\visual studio 2013\Projects\PortFolio\PortFolio\Content\Images\");//.Except("*.");
+
+            ViewBag.FN = fileNames;
+
+            ViewBag.ImageRP = "/Content/Images/";
+            
+            return View();
+        }
+    }
+}
